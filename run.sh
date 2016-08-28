806ad05fccbbf58bc9218c2ed4a02dc723e3c556
thegitfather <baranowski@joinpoint.org>
2016-08-28 21:51:55 +0200

    add run script

---
+++ run.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+BASENAME=`basename "$0"`
+
+if [ "$#" -ne 5 ]; then
+    echo -e "usage: \$ ./${BASENAME} [config] [user] [pw] [target] [port]\n"
+		exit 1
+fi
+
+CONFIG=$1
+USER=$2
+PW=$3
+TARGET=$4
+PORT=$5
+
+docker run -ti -p ${PORT}:${PORT} --privileged --rm debian-ike:0.1 ${CONFIG} ${USER} ${PW} ${TARGET} ${PORT}
+#docker run -d -p ${PORT}:${PORT} --privileged --name ike debian-ike:0.1 ${CONFIG} ${USER} ${PW} ${TARGET} ${PORT}
